48a56d98b91cdf1f9f3138455844c326a435c324
Lucile16 <lulubeucher@gmail.com>
2023-01-23 11:29:02 +0100

    first tests succeeded

---
+++ Bonjour_au_revoir/ohce.py
@@ -0,0 +1,6 @@
+class Ohce:
+    def miroir(self,mot):
+        miroir = mot[::-1]
+        if mot == miroir:
+            return miroir + " \n Bien dit"
+        return "Bonjour \n " + miroir + "\n Au revoir"
