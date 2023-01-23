fcb59b4d40d4dad7e7ac8c6c5503c5cc7b1c6ef9
Lucile16 <lulubeucher@gmail.com>
2023-01-23 11:00:12 +0100

    first test

---
+++ Bonjour_au_revoir/ohce_test.py
@@ -0,0 +1,42 @@
+import unittest
+import parameterized as parameterized
+from ohce import Ohce
+
+
+class OhceTest(unittest.TestCase):
+    # Test miroir
+    @parameterized.parameterized.expand(["toto", "test"])
+    def test_miroir(self,mot):
+        # QUAND on saisit une chaine
+        ohce = Ohce()
+        retour = ohce.miroir(mot)
+
+        # ALORS celle-ci est renvoyée en miroir
+        self.assertIn(mot[::-1], retour)
+
+    @parameterized.parameterized.expand(["sos", "laval"])
+    def test_palindrome(self,mot):
+        # QUAND on saisit un palindrome
+        ohce = Ohce()
+        retour = ohce.miroir(mot)
+
+        # ALORS celui-ci est renvoyé
+        retour_palindrome = ohce.miroir(mot)
+        self.assertIn(mot, retour_palindrome)
+
+        # ET 'Bien dit' est renvoyé ensuite
+        self.assertIn("Bien dit",retour_palindrome)
+
+    @parameterized.parameterized.expand(["toto", "test"])
+    def test_bonjour_revoir(self,mot):
+        # QUAND on saisit une chaine
+        ohce = Ohce()
+        retour = ohce.miroir(mot)
+
+        # ALORS 'Bonjour' est envoyé avant le mot et 'Au revoir' est envoyé après.
+        self.assertIn("Bonjour ", retour)
+        self.assertIn(" Au revoir", retour)
+
+
+if __name__ == '__main__':
+    unittest.main()
